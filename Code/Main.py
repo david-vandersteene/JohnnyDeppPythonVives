5c1883c7f22f6ae07e5361c51c7fc91758448b9c
david-vandersteene <david.vandersteene@student.vives.be>
2022-10-30 11:44:34 +0100

    init commit

---
+++ Code/Main.py
@@ -0,0 +1,39 @@
+class Vessel :
+    pass
+
+
+class Boat(Vessel):
+    pass
+
+
+class Ship(Vessel):
+    pass
+
+
+class SilentMary(Ship):
+    pass
+
+
+class BlackPearl(Ship):
+    pass
+
+
+class FishingBoat(Boat):
+    pass
+
+
+class RescueBoat(Boat):
+    pass
+
+
+class Pirate:
+    pass
+
+
+class Captain(Pirate):
+    pass
+
+
+class Civilian:
+    pass
+
