8a397d207a08b109520876b620189b9ac1de9e16
david-vandersteene <david.vandersteene@student.vives.be>
2022-10-30 12:12:00 +0100

    added the Main function to let the program run

--- Code/Main.py
+++ Code/Main.py
@@ -61,3 +61,85 @@ class Civilian:
         return self.name + "is a" + str(type(self))
 
 
+# main function:
+if __name__ == '__main__':
+    jackSparrow = Captain(name="Jack Sparrow", quote="This is the day you will always remember as the day you almost caught Captain Jack Sparrow!")
+    barbossa = Captain(name="Barbossa", quote="You Best Start Believin' In Ghost Stories... You're in one!")
+    salazar = Captain(name="Salazar", quote="My very dead men, the sea is ours! It's time to hunt a pirate!")
+
+    print(jackSparrow)
+    print(barbossa)
+    print(salazar)
+    print()
+
+    gibbs = Pirate("Joshamee Gibs")
+    will = Pirate("Will Turner")
+    elizabeth = Pirate("Elizabeth Swann")
+
+    print(gibbs)
+    print(will)
+    print(elizabeth)
+    print()
+
+    bootstrap = Pirate("Bootstrap Bill Turner")
+    pintel = Pirate("Pintel")
+    ragetti = Pirate("Ragetti")
+
+    print(bootstrap)
+    print(pintel)
+    print(ragetti)
+    print()
+
+    silentMary = SilentMary(img_path="img/silentmary.jpg")
+    blackPearl = BlackPearl(img_path="img/blackpearl.jpg")
+    fishingBoat = FishingBoat(img_path="img/fishingboat.jpg")
+    rescueBoat = RescueBoat()
+
+    print(silentMary)
+    print(blackPearl)
+    print(fishingBoat)
+    print(rescueBoat)
+    print()
+
+    silentMary.embark(salazar)
+    print(silentMary)
+    silentMary.sail((True, True))
+
+    blackPearl.embark(jackSparrow)
+    jackSparrow.add_crewmember(gibbs)
+    barbossa.add_crewmember(bootstrap)
+    barbossa.add_crewmember(pintel)
+    blackPearl.embark(barbossa)
+
+    print(blackPearl)
+    print(rescueBoat)
+    print(fishingBoat)
+    print()
+
+    blackPearl.sail((True, True))
+    blackPearl.disembark()
+    blackPearl.sail((True, True))
+    print()
+
+    rescueBoat.embark(will)
+    rescueBoat.embark(elizabeth)
+    print(rescueBoat)
+    rescueBoat.sail((True, True))
+    print(rescueBoat)
+    rescueBoat.disembark(elizabeth)
+    print(rescueBoat)
+    rescueBoat.disembark(ragetti)
+    rescueBoat.disembark(will)
+    print(rescueBoat)
+    print()
+
+    amberHeard = Civilian("Amber Heard")
+    print(amberHeard)
+    blackPearl.embark(amberHeard)
+    fishingBoat.embark(amberHeard)
+    print(fishingBoat)
+
+    silentMary.ShowImage()
+    #blackPearl.ShowImage()
+    #fishingBoat.ShowImage()
+    #rescueBoat.ShowImage()
